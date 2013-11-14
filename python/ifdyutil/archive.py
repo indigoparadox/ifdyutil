c52cbe5f4a49db8ae069a8e468d94dd95b0a98ed
indigoparadox <indigoparadox+bitbucket@interfinitdynamics.info>
2013-11-14 03:11:02 -0500

    Merged archive upgrade/create for efficiency.

--- python/ifdyutil/archive.py
+++ python/ifdyutil/archive.py
@@ -165,108 +165,64 @@ def handle( archive_path, key, salt=None ):
       ) )
       return None
 
-# TODO: Merge upgrade and create.
-
-def upgrade( archive_in_file, archive_out_path, new_key, new_salt=None ):
-
-   ''' This function should always write out the given archive in the current
-   format. '''
-
-   logger = logging.getLogger( 'ifdyutil.archive.upgrade' )
-
-   if not new_salt:
-      new_salt = Random.get_random_bytes( 160 )
-
-   # Copy the old archive to the new archive.
-   arcio = StringIO.StringIO()
-   with zipfile.ZipFile( arcio, 'w', zipfile.ZIP_DEFLATED ) as arcz_out:
-      for name in archive_in_file.filelist:
-         arcz_out.writestr( name, archive_in_file.read( name ) )
-
-   # Setup the encryptor. Expand and set the key.
-   iv = Random.get_random_bytes( 16 )
-   key_crypt = pbkdf2.PBKDF2( new_key, new_salt ).read( 32 )
-   encryptor = AES.new( key_crypt, AES.MODE_CBC, iv )
-
-   # Open the output file and start writing.
-   arcio.seek( 0, os.SEEK_SET )
-   with open( archive_out_path, 'wb' ) as archive_out_file:
-      archive_out_file.write( VERSIONS[len( VERSIONS ) - 1] )
-      archive_out_file.write( new_salt )
-      logger.debug( 'Salt generated: {}'.format(
-         base64.b64encode( new_salt )
-      ) )
-      archive_out_file.write( struct.pack( '<Q', len( arcio.getvalue() ) ) )
-      archive_out_file.write( iv )
-      # Process each chunk of the ZIP.
-      while True:
-         chunk = arcio.read( CHUNK_LEN )
-         if len( chunk ) == 0:
-            # We're done!
-            break
-         elif 0 != len( chunk ) % 16:
-            # Handle the trailing end.
-            chunk += ' ' * (16 - len( chunk ) % 16)
-         archive_out_file.write( encryptor.encrypt( chunk ) )
-
-def create( archive_path, key, salt=None, item_list=[] ):
+def create( archive_path, key, salt=None, item_list=[], index=True ):
 
    ''' Item list must be in the format:
-   [{'path_abs', 'path_rel, 'contents'}] '''
+   [{'path_rel, 'contents'}] '''
 
-   # TODO: Move this function to ifdyutil.
+   # TODO: Add archive_current status update stuff.
 
    logger = logging.getLogger( 'ifdyutil.archive.create' )
 
-   # Try to load the salt from a salt file.
-   # TODO: Add a versioning system to the file with salt in header.
-   # TODO: Port upgrade stuff from upgrade().
+   # Generate the salt if applicable.
    if not salt:
-      for salt_path in _salt_paths( archive_path ):
-         try:
-            with open( salt_path, 'r' ) as salt_file:
-               salt = salt_file.readline().strip()
-               logger.info( 'Salt found: {}'.format( salt_path ) )
-            break
-         except:
-            logger.warning( 'No salt found: {}'.format( salt_path ) )
+      salt = Random.get_random_bytes( 160 )
+      logger.debug( 'Salt generated: {}'.format(
+         base64.b64encode( salt )
+      ) )
 
-   # Create the search index for the archive.
-   schema = whoosh.fields.Schema(
-      path=whoosh.fields.ID(stored=True),
-      content=whoosh.fields.TEXT
-   )
-   ix_storage = whoosh.filedb.filestore.RamStorage()
-   ix = ix_storage.create_index( schema )
-   ix_writer = ix.writer()
+   if index:
+      # Create the search index for the archive.
+      schema = whoosh.fields.Schema(
+         path=whoosh.fields.ID(stored=True),
+         content=whoosh.fields.TEXT
+      )
+      ix_storage = whoosh.filedb.filestore.RamStorage()
+      ix = ix_storage.create_index( schema )
+      ix_writer = ix.writer()
    
    # Read all of the logs and write them to the archive ZIP.
    arcio = StringIO.StringIO()
    total_bytes = 0
    with zipfile.ZipFile( arcio, 'w', zipfile.ZIP_DEFLATED ) as arcz:
       for item in item_list:
-         logger.info( 'Indexing {}...'.format( item['path_rel'] ) )
-         ix_writer.add_document(
-            path=item['path_rel'][1:].decode( 'ascii' ),
-            content=item['contents']
-         )
+         # Index the item if applicable.
+         if index:
+            logger.info( 'Indexing {}...'.format( item['path_rel'] ) )
+            ix_writer.add_document(
+               path=item['path_rel'][1:],
+               content=item['contents']
+            )
+
+         # Store the item.
          logger.info( 'Storing {}...'.format( item['path_rel'] ) )
          arcz.writestr(
-            item['path_rel'], item['contents'].encode( 'utf-8' )
+            item['path_rel'], item['contents']
          )
          total_bytes += len( item['contents'] )
 
-      # Write the search index to the ZIP.
-      ix_writer.commit()
-      for ix_file_name in ix_storage.list():
-         with ix_storage.open_file( ix_file_name ) as ix_file:
-            logger.info( 'Storing {}...'.format( ix_file_name ) )
-            ix_contents = ix_file.read()
-            arcz.writestr(
-               os.path.join( '/index', ix_file_name ),
-               ix_contents
-            )
-            total_bytes += len( ix_contents )
+      if index:
+         # Write the search index to the ZIP.
+         ix_writer.commit()
+         for ix_file_name in ix_storage.list():
+            with ix_storage.open_file( ix_file_name ) as ix_file:
+               logger.info( 'Storing {}...'.format( ix_file_name ) )
+               ix_contents = ix_file.read()
+               arcz.writestr(
+                  os.path.join( '/index', ix_file_name ),
+                  ix_contents
+               )
+               total_bytes += len( ix_contents )
 
    logger.info( 'Stored {} bytes.'.format( total_bytes ) )
 
@@ -278,6 +234,8 @@ def create( archive_path, key, salt=None, item_list=[] ):
    # Open the output file and start writing.
    arcio.seek( 0, os.SEEK_SET )
    with open( archive_path, 'wb' ) as archive_file:
+      archive_file.write( VERSIONS[len( VERSIONS ) - 1] )
+      archive_file.write( salt )
       archive_file.write( struct.pack( '<Q', len( arcio.getvalue() ) ) )
       archive_file.write( iv )
       # Process each chunk of the ZIP.
